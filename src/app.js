5f1958ca529f0a84229c2ef377ef548e1a3a07ea
shiva994u <shiva994u@gmail.com>
2025-07-05 05:28:43 -0700

    update right UI domain

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'https://landingtest-adtech-ui.onrender.com', // http://localhost:5173
+  origin: "https://landingtest-adtech-ui.onrender.com", // http://localhost:5173
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
